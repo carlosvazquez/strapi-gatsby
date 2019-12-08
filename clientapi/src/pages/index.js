c1e22feb8b32c6e32bebf4fd3b2c3bc2951af840
Carlos Vazquez <carlosvazquezmurillo@gmail.com>
2019-12-08 13:33:01 -0600

    Add gatsby source strapi npm package

--- clientapi/src/pages/index.js
+++ clientapi/src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>API client</h1>
-    <p class="small">AA</p>
+    <p className="small">author: pending</p>
     <p>Welcome to your new Gatsby api client.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
